a1c69d832609c00c81a9e18f4b78ecd59ea88ba4
JWKennington <jameswkennington@gmail.com>
2020-10-26 18:44:57 -0400

    add test coverage for test init module

--- collapse/tests/test_collapse.py
+++ collapse/tests/test_collapse.py
@@ -2,8 +2,10 @@
 
 Presently only a dummy test to confirm repo setup and CI integration
 """
+import pathlib
 
 import collapse
+from collapse import tests
 
 
 class TestCollapse:
@@ -15,3 +17,7 @@ class TestCollapse:
         assert collapse.__MAJOR__ == EXPECTED[0], MSG.format(comp='MAJOR', exp=EXPECTED[0], got=collapse.__MAJOR__)
         assert collapse.__MINOR__ == EXPECTED[1], MSG.format(comp='MINOR', exp=EXPECTED[1], got=collapse.__MINOR__)
         assert collapse.__MICRO__ == EXPECTED[2], MSG.format(comp='MICRO', exp=EXPECTED[2], got=collapse.__MICRO__)
+
+    def test_test_root(self):
+        EXPECTED = pathlib.Path(__file__).parent.parent
+        assert tests.TEST_ROOT == EXPECTED, 'Collapse Test Directory moved. Expected {}, got {}'.format(EXPECTED.as_posix(), tests.TEST_ROOT.as_posix())
