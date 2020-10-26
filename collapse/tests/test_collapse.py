5fe6fc622d316f612da58cf8ddf5ff79d48872a3
JWKennington <jameswkennington@gmail.com>
2020-10-26 18:08:53 -0400

    bump package version

--- collapse/tests/test_collapse.py
+++ collapse/tests/test_collapse.py
@@ -10,7 +10,7 @@ class TestCollapse:
 
     def test_package_version(self):
         """Consistency test for version numbers"""
-        EXPECTED = (0, 0, 1)
+        EXPECTED = (0, 0, 2)
         MSG = 'Collapse Package {comp} Version Mismatch: Expected {exp:d}, got {got:d}'
         assert collapse.__MAJOR__ == EXPECTED[0], MSG.format(comp='MAJOR', exp=EXPECTED[0], got=collapse.__MAJOR__)
         assert collapse.__MINOR__ == EXPECTED[1], MSG.format(comp='MINOR', exp=EXPECTED[1], got=collapse.__MINOR__)
