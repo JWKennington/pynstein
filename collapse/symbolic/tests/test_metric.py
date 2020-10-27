837e19ccb11d34bebac97b340a10f6b8c35df34f
JWKennington <jameswkennington@gmail.com>
2020-10-27 04:03:53 -0400

    add metric utilities and tests

---
+++ collapse/symbolic/tests/test_metric.py
@@ -0,0 +1,124 @@
+"""Unittests for collapse package
+
+Presently only a dummy test to confirm repo setup and CI integration
+"""
+import pytest
+from sympy import Function, symbols, Derivative as D, Array
+
+from collapse.symbolic import metric, coords
+from collapse.symbolic.utilities import tensor_pow as tpow
+
+
+class TestMetric:
+
+    def _dummy_metric(self):
+        cs = coords.toroidal_coords(dim=2)
+        t, r = cs.base_symbols()
+        dt, dr = cs.base_oneforms()
+        a, b = symbols('a b')
+        form = a * tpow(dt, 2) + b * tpow(dr, 2)
+        return metric.Metric(twoform=form, components=(a, b))
+
+    def test_create_from_twoform(self):
+        g = self._dummy_metric()
+        assert isinstance(g, metric.Metric)
+
+    def test_create_from_matrix(self):
+        cs = coords.toroidal_coords(dim=2)
+        a, b = symbols('a b')
+        matrix = Array([[a, 0], [0, b]])
+        with pytest.raises(ValueError):
+            metric.Metric(matrix=matrix)
+        g = metric.Metric(matrix=matrix, coord_system=cs)
+        assert isinstance(g, metric.Metric)
+
+    def test_repr(self):
+        g = self._dummy_metric()
+        assert repr(g) == r"a*TensorProduct(dt, dt) + b*TensorProduct(dr, dr)"
+
+    def test_repr_latex(self):
+        g = self._dummy_metric()
+        assert g._repr_latex_() == '$\\displaystyle a \\operatorname{d}t \\otimes \\operatorname{d}t + b \\operatorname{d}r \\otimes \\operatorname{d}r$'
+
+    def test_properties(self):
+        g = self._dummy_metric()
+        _ = g.twoform
+        _ = g.matrix
+
+    def test_inverse(self):
+        g = self._dummy_metric()
+
+        # First, check cache is empty
+        assert g._inverse is None
+
+        # Then check matrix inverse computes
+        i = g.inverse
+        assert isinstance(i, metric.Metric)
+        assert repr(i) == "TensorProduct(dr, dr)/b + TensorProduct(dt, dt)/a"
+
+        # Last, check result cached
+        assert g._inverse is not None
+
+    def test_subs(self):
+        g = self._dummy_metric()
+        p = g.subs({g.components[0]: 0})
+        assert repr(p.twoform) == "b*TensorProduct(dr, dr)"
+        assert p.components == g.components[1:]
+
+
+class TestPredefinedMetrics:
+
+    def test_gim(self):
+        assert repr(metric.general_inhomogeneous_metric()) == ('-c**2*N(t, r)**2*TensorProduct(dt, dt) + L(t, r)**2*TensorProduct(c*M(t, '
+                                                               'r)*dt + dr, c*M(t, r)*dt + dr) + S(t, '
+                                                               'r)**2*(sin(theta)**2*TensorProduct(dphi, dphi) + TensorProduct(dtheta, '
+                                                               'dtheta))')
+
+    def test_flrw(self):
+        assert repr(metric.flrw_metric()) == ('-c**2*TensorProduct(dt, dt) + a(t)*(TensorProduct(dx, dx) + '
+                                              'TensorProduct(dy, dy) + TensorProduct(dz, dz))')
+
+
+class TestMetricDerivativeSimplification:
+
+    def test__deriv_simplify_rule(self):
+        """Consistency test for version numbers"""
+        t, r = symbols('t r')
+        F = Function('F')(t)
+        G = Function('G')(t, r)
+
+        assert metric._deriv_simplify_rule(F, t)[1].name == "F'"
+        assert metric._deriv_simplify_rule(F, (t, t))[1].name == "F''"
+        assert metric._deriv_simplify_rule(F, t, use_dots=True)[1].name == r"\dot{F}"
+        assert metric._deriv_simplify_rule(F, (t, t), use_dots=True)[1].name == r"\ddot{F}"
+        assert metric._deriv_simplify_rule(G, t)[1].name == "G_{t}"
+        assert metric._deriv_simplify_rule(G, (t, t))[1].name == "G_{t t}"
+        assert metric._deriv_simplify_rule(G, (t, r))[1].name == "G_{t r}"
+
+    def test_simplify_deriv_notation(self):
+        cs = coords.cartesian_coords()
+        t, x, *_ = cs.base_symbols()
+        dt, dx, *_ = cs.base_oneforms()
+        F = Function('F')(t)
+        G = Function('G')(t, x)
+
+        F_t = D(F, t)
+        F_tt = D(F_t, t)
+        G_t = D(G, t)
+        G_tt = D(G_t, t)
+        G_tr = D(G_t, x)
+
+        form = F ** 2 * tpow(dt, 2) + G ** 2 * tpow(dx, 2)
+        g = metric.Metric(twoform=form, components=(F, G))
+
+        # Check First-Order Terms
+        assert str(metric.simplify_deriv_notation(F_t, g, max_order=1)) == "F'(t)"
+        assert str(metric.simplify_deriv_notation(F_t, g, max_order=1, use_dots=True)) == r"\dot{F}(t)"
+
+        # Check Second-Order Pure Terms
+        assert str(metric.simplify_deriv_notation(F_tt, g, max_order=2)) == "F''(t)"
+        assert str(metric.simplify_deriv_notation(F_tt, g, max_order=2, use_dots=True)) == r"\ddot{F}(t)"
+
+        # Check Second-Order Mixed Terms
+        assert str(metric.simplify_deriv_notation(G_tt, g, max_order=2)) == "G_{t t}(t, x)"
+        assert str(metric.simplify_deriv_notation(G_tr, g, max_order=2)) == "G_{t x}(t, x)"
