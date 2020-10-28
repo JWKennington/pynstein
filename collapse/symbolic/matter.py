4d6004bb0292f5ab3af0813ee4174555df2601a0
JWKennington <jameswkennington@gmail.com>
2020-10-27 23:55:56 -0400

    docs and tests for matter

--- collapse/symbolic/matter.py
+++ collapse/symbolic/matter.py
@@ -1,6 +1,4 @@
-"""Utilities for constructing symbolic matter expressions, usually via
-the Stress-Energy Tensor
-
+"""Utilities for constructing symbolic matter expressions, usually via the Stress-Energy Tensor
 """
 
 from sympy import Array, symbols
@@ -10,6 +8,15 @@ from collapse.symbolic.metric import Metric
 
 
 def perfect_fluid_stress_energy(metric: Metric) -> Array:
+    """Compute the stress energy tensor for a perfect fluid in a given metric
+
+    Args:
+        metric:
+            Metric
+
+    Returns:
+        Array, the full stress energy tensor as a matrix
+    """
     p, rho = symbols('p rho')
     dim = metric.coord_system.dim
     return (p + rho) * diag(*[1, 0, 0, 0][:dim]) + p * metric.inverse.matrix
