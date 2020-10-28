155f0030e7fd925084a776f91ffa475dd2a3c494
JWKennington <jameswkennington@gmail.com>
2020-10-27 23:08:58 -0400

    document metric and fix reference to parent private attr

--- collapse/symbolic/metric.py
+++ collapse/symbolic/metric.py
@@ -1,12 +1,14 @@
 """Utilities for constructing a metric
 
 """
+
 import functools
 import itertools
 from typing import Tuple, Union
 
 from sympy import Function, sin, Expr, Array, Derivative as D, MatrixBase, Matrix
 from sympy.diffgeom import twoform_to_matrix
+from sympy.printing.latex import latex
 
 from collapse.symbolic import coords
 from collapse.symbolic.constants import c
@@ -14,7 +16,12 @@ from collapse.symbolic.utilities import tensor_pow as tpow, matrix_to_twoform
 
 
 class Metric:
+    """Metric represents a twoform on a manifold that is symmetric. This class is capable of being
+    created from and converted to both twoform notation and matrix notation given a coordinate system.
+    """
+
     def __init__(self, twoform: Expr = None, matrix: Array = None, coord_system: coords.CoordSystem = None, components: Tuple[Expr, ...] = None):
+        """Create a Metric"""
         if twoform is None and matrix is None:
             raise ValueError('Must specify either twoform or matrix to produce metric')
 
@@ -39,32 +46,45 @@ class Metric:
         self.components = components
 
     def __repr__(self):
+        """String repr"""
         return repr(self.twoform)
 
     def _repr_latex_(self):
-        return self.twoform._repr_latex_()
+        """LaTeX repr in Jupyter"""
+        s = latex(self.twoform, mode='plain')
+        return "$\\displaystyle %s$" % s
 
     @property
     def twoform(self):
+        """Safe accessor for twoform"""
         return self._twoform
 
     @property
     def matrix(self):
+        """Safe accessor for matrix"""
         return self._matrix
 
     @property
     def inverse(self):  # TODO include method parameters in here, for instance pseudo inverse
+        """Compute the inverse metric (if possible) and return new Metric instance"""
         if self._inverse is None:
             self._inverse = self.matrix.inv()  # only compute once
         return Metric(matrix=self._inverse, coord_system=self.coord_system, components=self.components)
 
     def subs(self, *args, **kwargs):
+        """Pass thru to twoform substitution"""
         return Metric(twoform=self.twoform.subs(*args, **kwargs),
                       # TODO make the filtering below more robust
                       components=tuple(c for c in self.components if not c.subs(*args, **kwargs).doit().is_constant()))
 
 
 def general_inhomogeneous_metric():
+    """Utility for constructing a general inhomogeneous, but still isotropic, metric. The metric
+    components M, N, L, S all depend on time and radius, but not theta or phi (hence isotropy).
+
+    Returns:
+        Metric, the GIM metric
+    """
     cs = coords.toroidal_coords()
     t, r, theta, _ = cs.base_symbols()
     dt, dr, dtheta, dphi = cs.base_oneforms()
@@ -82,6 +102,15 @@ def general_inhomogeneous_metric():
 
 
 def friedmann_lemaitre_roberston_walker_metric():
+    """Utility for constructing the FLRW metric in terms of a unit lapse and general
+    scale function `a`.
+
+    Returns:
+        Metric, the FLRW metric
+
+    References:
+        [1] S. Weinberg, Cosmology (Oxford University Press, Oxford ; New York, 2008).
+    """
     cs = coords.cartesian_coords()
     t, *_ = cs.base_symbols()
     dt, dx, dy, dz = cs.base_oneforms()
@@ -94,6 +123,7 @@ flrw_metric = friedmann_lemaitre_roberston_walker_metric  # shorthand for conven
 
 
 def _deriv_simplify_rule(component: Function, variables: Union[Expr, Tuple[Expr, ...]], use_dots: bool = False):
+    """Helper function for simplifying derivative notation"""
     if not isinstance(variables, tuple):  # TODO make this "boxing" more robust
         variables = (variables,)
     args = component.args
@@ -111,6 +141,23 @@ def _deriv_simplify_rule(component: Function, variables: Union[Expr, Tuple[Expr,
 
 
 def simplify_deriv_notation(expr: Expr, metric: Metric, max_order: int = 2, use_dots: bool = False):
+    """Utility for Simplifying LaTeX representation of a sympy Expression via substitutions. Note
+    that this function simplifies the LaTeX output of the Expr at the cost of the Expr semantics,
+    only use this after all operations on the Expr (including simplification) have been performed.
+
+    Args:
+        expr:
+            Expr, the sympy expression
+        metric:
+            Metric, the metric containing components whose derivatives will be simplified
+        max_order:
+            int, default 2, the max derivative order to replace
+        use_dots:
+            bool, default False. If True use dot notation for derivatives of single-variable functions
+
+    Returns:
+        Expr, the modified expression. Recall this is only useful for LaTeX conversion, not semantically valid in sympy.
+    """
     # Create Simplification Shorthand
     components = tuple(sorted(metric.components, key=lambda x: x.name))
     variables = metric.coord_system.base_symbols()
