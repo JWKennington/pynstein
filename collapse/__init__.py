fe6acf903933149156a50f80dd007ac4026c44c3
JWKennington <jameswkennington@gmail.com>
2020-10-28 01:10:59 -0400

    bump package version

--- collapse/__init__.py
+++ collapse/__init__.py
@@ -1,7 +1,7 @@
 """Package-level information"""
 __MAJOR__ = 0
-__MINOR__ = 0
-__MICRO__ = 2
+__MINOR__ = 1
+__MICRO__ = 0
 __VERSION__ = (__MAJOR__, __MINOR__, __MICRO__)
 __version__ = '.'.join(str(n) for n in __VERSION__)
 __github_url__ = 'https://github.com/JWKennington/collapse'
