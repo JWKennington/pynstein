31066a7ecd472c4fea846c646c20f8bb8a1ff505
JWKennington <jameswkennington@gmail.com>
2020-12-03 23:15:11 -0500

    version bump

--- collapse/__init__.py
+++ collapse/__init__.py
@@ -1,6 +1,6 @@
 """Package-level information"""
 __MAJOR__ = 0
-__MINOR__ = 1
+__MINOR__ = 2
 __MICRO__ = 0
 __VERSION__ = (__MAJOR__, __MINOR__, __MICRO__)
 __version__ = '.'.join(str(n) for n in __VERSION__)
